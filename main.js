6fa6f82c4a7f8f3629e7bd41e5b71dcf36e87203
sucrion <curcubeuu@gmail.com>
2022-04-01 23:53:51 -0400

    other page images added

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
+//MENU STUFF
 
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.display = "flex";
+  }
+  
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+} 
+
+//HEADER
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-100px";
+  }
+}
+
+//GRID TABS
+
+function openCity(evt, cityName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.firstElementChild.className += " w3-border-red";
+  }
